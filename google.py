81d8fac1891c018caf3c01162cfcd7683979e398
sebin-gang <sebin980911@naver.com>
2021-09-25 18:57:21 +0900

    1.0.0v

--- google.py
+++ google.py
@@ -6,7 +6,7 @@ import urllib.request
 driver = webdriver.Chrome()
 driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
 elem = driver.find_element_by_name("q")
-elem.send_keys("조코딩")
+elem.send_keys("xhaka face,lacazette face,aubameyang face,nicolas pepe face,berrelin face,ederson face,kyle walker face,ruben diaz face,zinchenco face,cancelo face,grealish face,kdb face,phil foden face,sterling face,gabriel jesus face,alisson becker face,van dijk face,robertson face,alexander-arnold face,james milner face,jordan henderson face,thiago alcantara face,firmino face,salah face,sadio mane face,origi face,ter stegen face,pique face,jordi alba face,umtiti face,busquets face,coutinho face,de jong face,ousmane dembele face,memphis depay face,sergio aguero face,courtios face,carvajar face,alaba face,marcelo face,kroos face,modric face,isco face,camavinga face,eden hazard face,benzema face,asensio face,bale face,vinicious jr face,oblak face,tripper face,koke face,marcos llorente face,joao felix face,griezmann face,suarez face,darmian face,vidal face,eriksen face,alexis sanchez face,dzeko face,bakayoko face,giroud face,zlatan face,morata face,chiellini face,de ligt face,danilo face,cuardrado face,sandro face,bonnuci face,arthur melo face,aaron ramsey face,federico chiesa face,dybala face,keylor navas face,hakimi face,ramos face,verratti face,di maria face,mbappe face,icardi face,neymar face,messi face,hummels face,reus face,erling haaland face,neuer face,upamecano face,niklas sule face,alphonso davies face,kimmich face,goretzka face,gnabry face,lewandowski face,leroy sane face,thomas muller face")
 elem.send_keys(Keys.RETURN)
 
 SCROLL_PAUSE_TIME = 1.2
--- google2.py
+++ google2.py
@@ -1,8 +1,7 @@
 from google_images_download import google_images_download
 
-response = google_images_download.googleimagesdownload()  # class instantiation
-
-arguments = {"keywords": "워너원 강다니엘,엑소 백현,박보검,송중기,워너원 황민현,엑소 시우민,강동원,이종석,이준기,마동석,조진웅,조세호,안재홍,윤두준,이민기,김우빈,육성재,공유,방탄소년단 정국,아이콘 바비,워너원 박지훈,엑소 수호",
-             "limit": 50, "print_urls": True, "format": "jpg"}  # creating list of arguments
-paths = response.download(arguments)  # passing the arguments to the function
-print(paths)  # printing absolute paths of the downloaded images
+response = google_images_download.googleimagesdownload()
+arguments = {"keywords" : "lacazette face,aubameyang face,nicolas pepe face,berrelin face,ederson face,kyle walker face,ruben diaz face,zinchenco face,cancelo face,grealish face,kdb face,phil foden face,sterling face,gabriel jesus face,alisson becker face,van dijk face,robertson face,alexander-arnold face,james milner face,jordan henderson face,thiago alcantara face,firmino face,salah face,sadio mane face,origi face,ter stegen face,pique face,jordi alba face,umtiti face,busquets face,coutinho face,de jong face,ousmane dembele face,memphis depay face,sergio aguero face,courtios face,carvajar face,alaba face,marcelo face,kroos face,modric face,isco face,camavinga face,eden hazard face,benzema face,asensio face,bale face,vinicious jr face,oblak face,tripper face,koke face,marcos llorente face,joao felix face,griezmann face,suarez face,darmian face,vidal face,eriksen face,alexis sanchez face,dzeko face,bakayoko face,giroud face,zlatan face,morata face,chiellini face,de ligt face,danilo face,cuardrado face,sandro face,bonnuci face,arthur melo face,aaron ramsey face,federico chiesa face,dybala face,keylor navas face,hakimi face,ramos face,verratti face,di maria face,mbappe face,icardi face,neymar face,messi face,hummels face,reus face,erling haaland face,neuer face,upamecano face,niklas sule face,alphonso davies face,kimmich face,goretzka face,gnabry face,lewandowski face,leroy sane face,thomas muller face,xhaka face",
+             "limit": 95, "print_urls": True, "format": "jpg"}
+paths = response.download(arguments)
+print(paths)  
