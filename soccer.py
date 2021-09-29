d4a2e6b96029b92c1a803d3f0629d5636ed27600
sebin-gang <sebin980911@naver.com>
2021-09-29 13:39:06 +0900

    python fixed

--- soccer.py
+++ soccer.py
@@ -1,7 +1,7 @@
 from bing_image_downloader import downloader
 
 
-downloader.download("angeldimaria", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("gerradpique", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
 
 
 
