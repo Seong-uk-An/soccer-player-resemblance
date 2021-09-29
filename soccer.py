2dee033e50a1a3d72861aef344bf7a85ec2da3ad
sebin-gang <sebin980911@naver.com>
2021-09-29 16:23:50 +0900

    sentence

--- soccer.py
+++ soccer.py
@@ -1,7 +1,14 @@
 from bing_image_downloader import downloader
 
-
-downloader.download("gerradpique", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("eden hazard", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("deGea", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("jadon sancho", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("olivier giroud", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("gnabry", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("goretzka", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("harry Maguire", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("anthony martial", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+downloader.download("vidal", limit=100,  output_dir='dataset2', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
 
 
 
