1f3d78aa206e73c0e15f05e3dbd8c37e0f6603af
sebin-gang <sebin980911@naver.com>
2021-09-30 15:09:49 +0900

    add profile

--- sentence.js
+++ sentence.js
@@ -1,430 +1,537 @@
 const sentences = [
   {
     name: "손흥민",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "뤼디거",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "알라바",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "아놀드",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "알렉시스 산체스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "알리송",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "아센시오",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "오바메양",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "아스필리쿠에타",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "바카요코",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "가레스 베일",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "벤제마",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "브루노 페르난데스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "브라이언 힐",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "부카요 샤카",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "부스케츠",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "카미빙가",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "카바니",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "키엘리니",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "쿠르투아",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "쿠티뉴",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "다르미안",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "데 리흐트",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "델레 알리",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "디발라",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "에딘 제코",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "에데르송",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "에릭 다이어",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "에릭센",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "엘링 홀란드",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "키에사",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "피르미누",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "프랭키 데용",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "가브리엘 제수스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "그리즈만",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "하키미",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "해리 케인",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "베예린",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "요리스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "이카르디",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "이스코",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "제임스 밀너",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "주앙 펠릭스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "헨더슨",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "호르디 알바",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "조르지뉴",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "카이 하베르츠",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "캉테",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "케파",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "케빈 데 브라이너",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "나바스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "키미히",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "코케",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "토니 크로스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "디 마리아",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "라카제트",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "라멜라",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "레노",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "사네",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "레반도프스키",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "루카스 모우라",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "루카쿠",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "루크 쇼",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "마르셀루",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "마르코스 알론소",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "마샬",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "메이슨 마운트",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "음바페",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "데파이",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "메시",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "모드리치",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "모라타",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "노이어",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "네이마르",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "필 포든",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "포그바",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "라모스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "래쉬포드",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "로이스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "로버트슨",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "호날두",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "후벵 디아스",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "사디오 마네",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "살라",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "아구에로",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "레길론",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "스털링",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "수아레즈",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "테어 슈테겐",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "피케",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "티아고 알칸타라",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "티아고 실바",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "토마스 뮐러",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "티모 베르너",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "트리피어",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "움티티",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "우파메카노",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "반 더 비크",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "반 데이크",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "바란",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "베라티",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "비니시우스 주니오르",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "자카",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "진첸코",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "즐라탄 이브라히모비치",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "그릴리쉬",
+    profile: "",
     sentences: "당신은 ~~~",
   },
   {
     name: "린가드",
+    profile: "",
     sentences: "당신은 ~~~",
   },
 ];
